55695000222f6c881539f4f18aa23d153a9c2298
binbows02 <sammy29032006@gmail.com>
2023-12-05 20:54:57 +0100

    0.04

--- main menu/main menu.js
+++ main menu/main menu.js
@@ -34,3 +34,35 @@ function sfx() {
 
     cursor = pointer;
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    const openModalButtons = document.querySelectorAll('[data-modal-target]')
+    const closeModalButtons = document.querySelectorAll('[data-close-button]')
+    const overlay = document.getElementById('blackoverlay')
+
+    openModalButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const modal = document.querySelector(button.dataset.modalTarget)
+            openModal(modal)
+        })
+    })
+
+    closeModalButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const modal = button.closest('.modal')
+            closeModal(modal)
+        })
+    })
+});
+
+function openModal(modal) {
+    if (modal == null) return
+    modal.classList.add('active')
+    blackoverlay.classList.add('active')
+}
+
+function closeModal(modal) {
+    if (modal == null) return
+    modal.classList.remove('active')
+    blackoverlay.classList.remove('active')
+}
